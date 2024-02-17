cafea455a3fa6d4b2e014411049726ed5b4f3500
lukeymjobe <lukeymjobe@gmail.com>
2024-02-17 15:01:02 -0800

    triple jump
    When the Pacman lands on a platform occupied by the penguin, the penguin destroys said platform. The Pacman can now ground-smash and most importantly, can triple-jump. We also scaled up the game specifically for macbook pro.

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import pygame
 
 pygame.init()
 from pygame.locals import *
+import random
 from time import sleep, time
 
 CLOCK = pygame.time.Clock()
 running = True
-WIDTH = 1200
-HEIGHT = 600
+WIDTH = 1600
+HEIGHT = 900
 vec = pygame.math.Vector2
 
 background = pygame.transform.scale(pygame.image.load("desert.jpg"), (WIDTH, HEIGHT))
@@ -31,7 +32,7 @@ class Pacman(pygame.sprite.Sprite):
         self.rect.x = 400
         self.rect.y = 235
 
-        self.ACC = 0.5
+        self.ACC = 0.9
         self.FRIC = -0.12
 
         self.pos = vec((420, 235))
@@ -39,6 +40,7 @@ class Pacman(pygame.sprite.Sprite):
         self.acc = vec(0, 0)
 
         self.double_jump = False
+        self.triple_jump = False
 
     def update(self):
         self.acc = vec(0, 0.5)
@@ -81,9 +83,15 @@ class Pacman(pygame.sprite.Sprite):
         if hits:
             self.vel.y = -15
             self.double_jump = False
+            self.triple_jump = False
         if not hits:
             if not self.double_jump:
                 self.vel.y = -15
+                self.double_jump = True
+                self.triple_jump = False
+            elif self.double_jump and not self.triple_jump:
+                self.vel.y = -15
+                self.triple_jump = True
                 self.double_jump = True
 
     def render(self):
@@ -93,10 +101,36 @@ class Pacman(pygame.sprite.Sprite):
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height):
         super().__init__()
+        self.width = width
+        self.height = height
         self.surf = pygame.Surface((width, height))
         self.surf.fill((255, 0, 0))
         self.rect = self.surf.get_rect(center=(WIDTH / 2, HEIGHT - 10))
         self.rect.x, self.rect.y = x, y
+        self.active = True
+
+    def update(self):
+        temp = pygame.sprite.Group()
+        temp.add(self)
+
+        if pygame.Rect.colliderect(pacman.rect, self.rect) and pygame.sprite.spritecollide(penguin, temp,
+                                                                                           False) and len(
+                platforms.sprites()) > 2:
+            self.active = False
+            platforms.remove(self)
+
+            # randomly pick another platform to move penguin to
+            index = random.randint(0, len(platforms.sprites()) - 2)
+            newPlatform = platforms.sprites()[index]
+            penguin.pos = vec((newPlatform.rect.x + (newPlatform.width / 2), newPlatform.rect.y - 30))
+
+            penguin.left_boundary = newPlatform.rect.x
+            penguin.right_boundary = (newPlatform.rect.x + newPlatform.width) - 100
+
+        if pygame.Rect.colliderect(pacman.rect, self.rect):
+            keypress = pygame.key.get_pressed()
+            if keypress[K_s] and pacman.double_jump:
+                platforms.remove(self)
 
     def render(self):
         DISPLAY.blit(self.surf, self.rect)
@@ -107,7 +141,6 @@ class Penguin(pygame.sprite.Sprite):
         super().__init__()
         self.walking_images_r = []
         self.walking_images_l = []
-
 
         self.dying_images = []
 
@@ -130,8 +163,6 @@ class Penguin(pygame.sprite.Sprite):
             img_with_flip = pygame.transform.flip(image, True, False)
             self.walking_images_l.append(img_with_flip)
 
-
-
         self.ACC = 0.2
         self.FRIC = -0.12
 
@@ -144,13 +175,15 @@ class Penguin(pygame.sprite.Sprite):
         self.image = self.walking_images_r[self.walking_index]
         self.rect = self.image.get_rect()
 
+        self.left_boundary = 300
+        self.right_boundary = 500
 
     def update(self):
         self.acc = vec(0, 0.5)
 
-        if self.rect.x <= 200:
+        if self.rect.x <= self.left_boundary:
             self.direction = "RIGHT"
-        if self.rect.x >= 450:
+        if self.rect.x >= self.right_boundary:
             self.direction = "LEFT"
 
         if self.direction == "RIGHT":
@@ -170,7 +203,6 @@ class Penguin(pygame.sprite.Sprite):
         else:
             self.image = self.walking_images_l[self.walking_index]
 
-
         # gravity
         hits = pygame.sprite.spritecollide(penguin, platforms, False)
         if hits:
@@ -186,11 +218,15 @@ DISPLAY = pygame.display.set_mode((WIDTH, HEIGHT))
 pacman = Pacman()
 penguin = Penguin()
 
-platform = Platform(200, 400, 300, 20)
-platform1 = Platform(400, 300, 100, 20)
-platform2 = Platform(600, 500, 100, 20)
-platform3 = Platform(800, 200, 100, 20)
-ground = Platform(0, 580, 1200, 20)
+platform = Platform(300, 400, 600, 20)
+platform1 = Platform(400, 300, 870, 20)
+platform2 = Platform(600, 500, 460, 20)
+platform3 = Platform(800, 200, 740, 20)
+platform4 = Platform(75, 200, 480, 20)
+platform5 = Platform(900, 66, 740, 20)
+platform6 = Platform(400, 500, 740, 20)
+
+ground = Platform(0, HEIGHT, WIDTH, 20)
 
 platforms = pygame.sprite.Group()
 
@@ -198,6 +234,9 @@ platforms.add(platform)
 platforms.add(platform1)
 platforms.add(platform2)
 platforms.add(platform3)
+platforms.add(platform4)
+platforms.add(platform5)
+platforms.add(platform6)
 platforms.add(ground)
 
 while running:
@@ -220,6 +259,9 @@ while running:
 
     penguin.update()
     penguin.render()
+
+    for i in platforms:
+        i.update()
 
     for i in platforms:
         i.render()
